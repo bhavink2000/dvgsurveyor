d4155719d2786239905ffd635133c22ba0fa04db
Bhavin Khunt <bhavinskhunt@gmail.com>
2025-07-06 10:14:53 +0530

    feat: make dashboard screen to nevigate from login

--- lib/app_routes/app_routes.dart
+++ lib/app_routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:dvgsurveyor/src/dashboard/dashboard.dart';
 import 'package:dvgsurveyor/src/login_screen/login_screen.dart';
 import 'package:dvgsurveyor/src/register_screen/register_screen.dart';
 import 'package:dvgsurveyor/src/splash_screen/splash_screen.dart';
@@ -5,10 +6,9 @@ import 'package:dvgsurveyor/src/welcome_screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
-
   static const String splashScreen = '/splash-screen';
   static const String welcomeScreen = '/welcome-screen';
-  static const String homeScreen = '/home-screen';
+  static const String dashScreen = '/dashboard-screen';
   static const String loginScreen = '/login-screen';
   static const String registerScreen = '/register-screen';
 
@@ -17,16 +17,15 @@ class AppRoutes {
       case splashScreen:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case welcomeScreen:
-       return MaterialPageRoute(builder: (_) => const WelcomeScreen());
-      // case homeScreen:
-      //   return MaterialPageRoute(builder: (_) => const HomeScreen());
+        return MaterialPageRoute(builder: (_) => const WelcomeScreen());
       case loginScreen:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
       case registerScreen:
         return MaterialPageRoute(builder: (_) => const RegisterScreen());
+      case dashScreen:
+        return MaterialPageRoute(builder: (_) => const DashboardScreen());
       default:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
     }
   }
- 
 }
