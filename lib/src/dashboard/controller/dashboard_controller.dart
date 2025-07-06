cdfad111ebdc3a502d2d8ef6df2aab4319cd3bc4
Bhavin Khunt <bhavinskhunt@gmail.com>
2025-07-06 12:54:16 +0530

    feat: dashboard screen setup and get username from session

--- lib/src/dashboard/controller/dashboard_controller.dart
+++ lib/src/dashboard/controller/dashboard_controller.dart
@@ -1,6 +1,12 @@
-
+import 'package:dvgsurveyor/session_manager/session_manger.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final dashboardController = Provider((ref)=> DashboardController());
+final dashboardController = Provider((ref) => DashboardController());
 
-class DashboardController {}
+class DashboardController {
+  String userName = '';
+  
+  void getUserDataFromSesstion() async {
+    userName = await SessionManager.getUsername() ?? '';
+  }
+}
--- lib/src/dashboard/dashboard.dart
+++ lib/src/dashboard/dashboard.dart
@@ -1,24 +1,87 @@
+import 'package:dvgsurveyor/helper/app_colors.dart';
+import 'package:dvgsurveyor/helper/app_const.dart';
+import 'package:dvgsurveyor/helper/app_fonts_helper.dart';
+import 'package:dvgsurveyor/helper/app_padding.dart';
+import 'package:dvgsurveyor/src/dashboard/controller/dashboard_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class DashboardScreen extends ConsumerStatefulWidget{
+class DashboardScreen extends ConsumerStatefulWidget {
   const DashboardScreen({super.key});
 
   @override
   ConsumerState<DashboardScreen> createState() => _DashboardScreenState();
 }
+
 class _DashboardScreenState extends ConsumerState<DashboardScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the controller to fetch user data
+    final controller = ref.read(dashboardController);
+    controller.getUserDataFromSesstion();
+  }
+
   @override
   Widget build(BuildContext context) {
     // final state = ref.watch(dashboardController);
-    // final controller = ref.read(dashboardController.notifier);
+    final controller = ref.read(dashboardController);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Dashboard!'),
+      backgroundColor: AppColors.tealPrimary,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.menu, color: AppColors.offWhite),
+                ),
+                Text(
+                  AppConst.appName,
+                  style: AppFonts.text20(context).copyWith(
+                    color: AppColors.offWhite,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            SizedBox(height: 4.h),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome, ${controller.userName}',
+                  style: AppFonts.text20(context).copyWith(
+                    color: AppColors.offWhite,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.h),
+                Text(
+                  'Here is your dashboard',
+                  style: AppFonts.text16(context).copyWith(
+                    color: AppColors.offWhite,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ).paddingOnly(left: 16.w),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.offWhite,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
