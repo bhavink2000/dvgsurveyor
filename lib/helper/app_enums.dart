b18b9f536f8609cbb6e253be4e672b9412c15683
Bhavin Khunt <bhavinskhunt@gmail.com>
2025-07-07 18:52:59 +0530

    feat: make funcation for all user get in repo , navigate user mangement screen in app routes and make enums

--- lib/helper/app_enums.dart
+++ lib/helper/app_enums.dart
@@ -1,14 +1,21 @@
-enum RoleEnum { superAdmin, admin, worker }
+import 'package:get/get.dart';
+
+enum RoleEnum { admin, worker }
 
 extension RoleEnumExtension on RoleEnum {
   String get displayName {
     switch (this) {
-      case RoleEnum.superAdmin:
-        return 'Super Admin';
       case RoleEnum.admin:
         return 'Admin';
       case RoleEnum.worker:
         return 'Worker';
     }
+  }
+
+  static RoleEnum? fromString(String? value) {
+    if (value == null) return null;
+    return RoleEnum.values.firstWhereOrNull(
+      (e) => e.displayName.toLowerCase() == value.toLowerCase(),
+    );
   }
 }
