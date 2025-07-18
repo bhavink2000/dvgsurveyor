ebc098d679b2d4d1e2ce2c0abf14f52990c61212
Bhavin Khunt <bhavinskhunt@gmail.com>
2025-07-18 13:12:52 +0530

    feat: city, datetime, download png add

--- lib/helper/app_images_helper.dart
+++ lib/helper/app_images_helper.dart
@@ -6,4 +6,8 @@ class AppImages {
   static const String dvgSLogo = 'assets/images/dvg_surveyor_logo.png';
   static const String dvgWel = 'assets/images/dvg_wel_logo.png';
   static const String dvgWelLottie = 'assets/gif_image/dvg_wel_lottie.json';
+
+  static const String cityWise = 'assets/images/city_wise.png';
+  static const String dateWise = 'assets/images/date_wise.png';
+  static const String excelDownload = 'assets/images/excel_download.png';
 }
